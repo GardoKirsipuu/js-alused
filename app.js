4ebafce2ce2d2a90db07e9bf965247a92fe25ee3
Gardo Kirsipuu <gardo1999@gmail.com>
2021-09-24 10:02:51 +0300

    objektid

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
-const numbers1 = [55, 32, 22, 86, 99, 1, 11, 66];
-const numbers2 = new Array(55, 32, 22, 86, 99, 1, 11, 69);
-const fruits = ["Õun", "Banaan", "Pirn"];
-const mixed = [22, "Tere", true, undefined, null, {a:1, b:2}, new Date()];
+const person = {
+	firstName: "Kadi",
+	lastname: "Tamm",
+	age: 36,
+	email: "kadi.tamm@gmail.com",
+	hobbies: ["muusika", "sport"],
+	address: {
+		city: "Tallinn",
+		county: "Harjumaa"
+	},
+	getBirthYear: function() {
+		return 2021 - this.age;
+	}
+};
 
 let val;
 
-val = numbers1.length; //massiivi pikkus
-val = Array.isArray(numbers1); // kas on massiiv
+val = person;
 
-val = numbers1[0]; // kindel element
-val = numbers1[2];
+val = person.firstName;
+val = person["lastname"];
+val = person.age;
+val = person.hobbies[1];
+// val = person.address.city;
+val = person.address["city"];
+val = person.getBirthYear();
 
-numbers1[2] = 100; //elemendi lisamine
+const people = [
+	{name: "Kadi", age: 36},
+	{name: "Mati", age: 38},
+	{name: "Mike", age: 23},
+];
 
-val = numbers1.indexOf(66); // elemendi järgi index
+val = people;
 
-numbers1.push(250); // elemendi massiivi lõppu lisamine
-numbers1.unshift(120); // elemendi massiivi algusesse lisamine
-numbers1.pop(); // elemendi kustutamine massiivi lõpust
-numbers1.shift(); // elemendi kustutamine massiivi algusest
-
-// console.log(numbers1);
-
-// numbers1.splice(1, 3); // massiivi alates index 1 3 elementi kustutamine
-
-// numbers1.reverse(); // massiivi tagurpidi keeramine
-
-val = numbers1.concat(numbers2); // kahe massiivi kokku panemine
-
-val = fruits.sort(); // massiivi sorteerimine
-
-console.log(numbers1);
-
-// massiivi sorteerimine suurem-> väiksem ja vastupidi
-
-val = numbers1.sort(function(x, y){
-	return x - y;
-})
-
-val = numbers1.sort(function(x, y){
-	return y - x;
-})
+for(let i = 0; i < people.length; i++){
+	console.log(people[i].name);
+};
 
 console.log(val);
