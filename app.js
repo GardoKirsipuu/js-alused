cf9b66724326e87f508bb65c4124efabd0d9267f
Gardo Kirsipuu <gardo1999@gmail.com>
2021-09-24 09:43:17 +0300

    Array and methods

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-const name = "Gardo Kirsipuu";
-const age = 21;
-const job = "õpilane";
-const city = "Mustvee";
+const numbers1 = [55, 32, 22, 86, 99, 1, 11, 66];
+const numbers2 = new Array(55, 32, 22, 86, 99, 1, 11, 69);
+const fruits = ["Õun", "Banaan", "Pirn"];
+const mixed = [22, "Tere", true, undefined, null, {a:1, b:2}, new Date()];
 
-let html;
+let val;
 
-// es5 standard
+val = numbers1.length; //massiivi pikkus
+val = Array.isArray(numbers1); // kas on massiiv
 
-html = "<ul>" +
-			"<li>" + name + "</lI>" +
-			"<li>" + age + "</lI>" +
-			"<li>" + job + "</lI>" +
-			"<li>" + city + "</lI>" +
-		"</ul>";
+val = numbers1[0]; // kindel element
+val = numbers1[2];
 
-// es6 - template strings
+numbers1[2] = 100; //elemendi lisamine
 
-html = `
-		<ul>
-			<li>Name: ${name}</li>
-			<li>Age: ${age}</li>
-			<li>Job: ${job}</li>
-			<li>City: ${city}</li>
-		</ul>
-`;
+val = numbers1.indexOf(66); // elemendi järgi index
 
-console.log(html);
+numbers1.push(250); // elemendi massiivi lõppu lisamine
+numbers1.unshift(120); // elemendi massiivi algusesse lisamine
+numbers1.pop(); // elemendi kustutamine massiivi lõpust
+numbers1.shift(); // elemendi kustutamine massiivi algusest
 
-document.body.innerHTML = html;
+// console.log(numbers1);
+
+// numbers1.splice(1, 3); // massiivi alates index 1 3 elementi kustutamine
+
+// numbers1.reverse(); // massiivi tagurpidi keeramine
+
+val = numbers1.concat(numbers2); // kahe massiivi kokku panemine
+
+val = fruits.sort(); // massiivi sorteerimine
+
+console.log(numbers1);
+
+// massiivi sorteerimine suurem-> väiksem ja vastupidi
+
+val = numbers1.sort(function(x, y){
+	return x - y;
+})
+
+val = numbers1.sort(function(x, y){
+	return y - x;
+})
+
+console.log(val);
