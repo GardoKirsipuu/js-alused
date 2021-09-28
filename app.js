44a07fe4b2114750741c1d15ab03957375237a22
Gardo Kirsipuu <gardo1999@gmail.com>
2021-09-28 10:19:39 +0300

    Todo list in webpage: my version

--- app.js
+++ app.js
@@ -1,52 +1,5 @@
-// funktsiooni defineerimine (deklareerimine)
-function greeting(firstname = "Kadi", lastname = "Tamm"){
-
-	/*
-	if(typeof firstname == "undefined"){
-		firstname = "Kadi";
-	}
-	if(typeof lastname == "undefined"){
-		lastname = "Tamm";
-	}
-	*/
-	return "Tere, " + firstname + " " + lastname;
+function lisamine(){
+	let b = document.getElementById("lisaAdd").value;
+	console.log(b);
+	document.getElementById("list").innerHTML += `<li>${b}</li>`;
 }
-// funktsiooni kutsumine tööle
-
-let greet = greeting("Gardo", "Kirsipuu");
-console.log(greet);
-
-console.log();
-// funktsioon avaldise kujul
-const square = function(number = 3){
-	return number * number;
-}
-console.log(square());
-
-// IIRE - Immediately Invoked Function Expression
-(function(){
-	console.log("function is ran")
-})();
-
-(function(name){
-	console.log("Tere " + name)
-})("Gardo");
-
-console.log();
-// objekti sees defineeritud funktsioon
-
-const todo = {
-	add: function(){
-		console.log("add todo......")
-	},
-	edit: function(id){
-		console.log(`edit todo ${id}`)
-	},
-	delete: function(){
-		console.log("delete todo......")
-	}
-}
-
-todo.add();
-todo.edit(5);
-todo.delete();
