7344db2d5aad56b2968b470af7556ccc59b2fc8b
Gardo Kirsipuu <gardo1999@gmail.com>
2021-09-28 11:21:11 +0300

    Document object model

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-function lisamine(){
-	let b = document.getElementById("lisaAdd").value;
-	console.log(b);
-	document.getElementById("list").innerHTML += `<li>${b}</li>`;
-}
+let val;
+
+
+val = document;
+val = document.all;
+val = document.all[5];
+val = document.all.length;
+
+val = document.head;
+val = document.body;
+
+val = document.doctype;
+
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].method;
+val = document.forms[0].action;
+val = document.forms[0].id;
+
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.scripts;
+val = document.scripts[0];
+val = document.scripts[1];
+val = document.scripts[1].getAttribute("src");
+
+console.log(val);
