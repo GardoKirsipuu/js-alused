6340ca34f4588903149f73e7516588d971e64984
Gardo Kirsipuu <gardo1999@gmail.com>
2021-09-29 14:42:57 +0300

    DOM selector for multiple elements

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
 let val;
 
-// document.getElementById('')
+val = document.querySelectorAll("li");
 
-val = document.getElementById('tasks-title');
-val = document.getElementById('tasks-title').id;
-val = document.getElementById('tasks-title').className;
+const oddLi = document.querySelectorAll(`li:nth-child(odd)`);
 
-const tasksTitle = document.getElementById('tasks-title');
+const evenLi = document.querySelectorAll(`li:nth-child(even)`);
 
-// stiili muutmine
+oddLi.forEach(function(li){
+	console.log(li)
+	li.style.background = "grey"
+})
 
-tasksTitle.style.background = "#333"
-tasksTitle.style.color = "#fff"
-tasksTitle.style.padding = "5px"
-// tasksTitle.style.display = "none"
+for(let i = 0; i < evenLi.length; i++){
+	evenLi[i].style.background = "green"
+}
 
-// sisu muutmine
-tasksTitle.textContent = "My tasks"
-tasksTitle.innerText = "My favourite tasks"
-//tasksTitle.innerHTML = "<span style="color: red;">My Tasks</span>";
-
-// document.querySelector
-
-val = document.querySelector("#tasks-title")
-val = document.querySelector(".card-title")
-val = document.querySelector("h2")
-
-document.querySelector("li").style.color = "red"
-
-document.querySelector("ul li").style.color = "blue"
-
-document.querySelector("li:last-child").style.color = "green"
-
-document.querySelector("ul li").style.color = "blue"
-
-document.querySelector("li:nth-child(odd)").style.background = "#ccc"
-
-document.querySelector("li:nth-child(even)").style.background = "#f4f4f4"
-
-document.querySelector("li:last-child").style.background = "#ccc"
-
-console.log(val);
+// console.log(evenLi);
