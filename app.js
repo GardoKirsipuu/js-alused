6b3d43bd766c36547a496eb159d2497754d5ba3c
Gardo Kirsipuu <gardo1999@gmail.com>
2021-10-08 13:42:48 +0300

    Remove task ALL DONE

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 // event elements
 const taskList = document.querySelector('ul')
+const remAll = document.getElementById('remAllbtn')
 
 // taskList.remove();
 
 // click
 taskList.addEventListener('click', removeFromList)
+remAll.addEventListener('click', removeAll)
 
 function removeFromList(e) {
-	const target = event.target.parentElement;
-	target.remove();
+	if (e.target.textContent == 'X') {
+		if (confirm('Are you sure you want to delete this task?')) {
+			e.target.parentElement.remove();
+		}
+	}
+}
+
+function removeAll(e) {
+	if (confirm('Are you sure you want to delete all task?')) {
+		if (e.target.id == 'remAllbtn') {
+			taskList.innerHTML = '';
+		}
+	}
 }
