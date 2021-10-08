e0ce7d4baee8a33277a95696f5a59f11cf7e57c4
Gardo Kirsipuu <gardo1999@gmail.com>
2021-10-08 13:58:05 +0300

    Remove task + add task OK

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 // event elements
+const form = document.querySelector('form');
+const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('ul')
 const remAll = document.getElementById('remAllbtn')
 
-// taskList.remove();
+// submit 
+form.addEventListener('submit', addToList)
+
+function addToList(e) {
+	const li = document.createElement('li');
+	const list = document.querySelector('ul');
+
+	li.className = 'collection-item';
+	li.appendChild(document.createTextNode(taskInput.value));
+
+	const link = document.createElement('a');
+	link.className = 'secondary-content'
+	link.appendChild(document.createTextNode('X'));
+	link.setAttribute('href', '#')
+
+	li.appendChild(link);
+	list.appendChild(li);
+
+	taskInput.value = '';
+
+	e.preventDefault();
+}
 
 // click
 taskList.addEventListener('click', removeFromList)
